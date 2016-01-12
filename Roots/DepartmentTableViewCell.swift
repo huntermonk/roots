7f8362199c0950163c7b38e46fce62d82938dc7a
Hunter <hunter.monk@gmail.com>
2016-01-12 15:18:47 -0600

    removed extraneous code

--- Roots/DepartmentTableViewCell.swift
+++ Roots/DepartmentTableViewCell.swift
@@ -11,15 +11,5 @@ import UIKit
 class DepartmentTableViewCell: UITableViewCell {
 
     @IBOutlet weak var title: UILabel!
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-        // Configure the view for the selected state
-    }
 
 }
